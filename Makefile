5965c04f0c70d394d4c5c38b265be0c3ef93fa7d
Simone Ripamonti <simone.ripamonti@bitrock.it>
2024-02-22 16:22:47 +0100

    Updates

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 package:
 	mvn install
 
-docker-build: package
-	docker build -t bitripa/springboot-sample-app:latest .
-
-docker-push:
-	docker push bitripa/springboot-sample-app:latest
+docker: package
+	docker build -t bitripa/springboot-sample-app:$(version) .
+	docker push bitripa/springboot-sample-app:$(version)
--- src/main/java/it/bitrock/codemotionworkshopdemo/CodemotionWorkshopDemoApplication.java
+++ src/main/java/it/bitrock/codemotionworkshopdemo/CodemotionWorkshopDemoApplication.java
@@ -1,5 +1,7 @@
 package it.bitrock.codemotionworkshopdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CodemotionWorkshopDemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(CodemotionWorkshopDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CodemotionWorkshopDemoApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello() {
+		logger.info("Answering to an Hello");
 		return "Hello";
 	}
 }
